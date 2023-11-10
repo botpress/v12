51dd5ada6aaabaf57d22da100bfb20f38aec4baa
David Vitor Antonio <david.vitora@gmail.com>
2023-11-10 20:44:47 -0300

    add keepAliveTimeout

--- packages/bp/src/core/app/server.ts
+++ packages/bp/src/core/app/server.ts
@@ -241,6 +241,15 @@ export class HTTPServer {
     app.use(process.ROOT_PATH, this.app)
     this.httpServer = createServer(app)
 
+    const { keepAliveTimeout } = botpressConfig.httpServer
+
+    console.log({ keepAliveTimeout })
+
+    if (keepAliveTimeout && Number.isInteger(keepAliveTimeout)) {
+      console.log('will use: of ' + { keepAliveTimeout })
+      this.httpServer.keepAliveTimeout = keepAliveTimeout
+    }
+
     await this.mediaRouter.initialize()
 
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
